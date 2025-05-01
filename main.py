a2426ab7a621ddccbc17d583b440e56745cd4d0f
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-05-01 20:39:58 +0200

    add escape key to exit program on rpi

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import gFrame as gf
 from src import data
 from gFrame import vars
 from src.enums import pages
+from pygame import K_ESCAPE
 
 # 200 doesn't matter and is overwritten on the line below
 data.APP = gf.AppConstructor(data.APP_WIDTH, 800)
@@ -22,4 +23,8 @@ page_listing = [None, None, None, page, None]
 if __name__ == "__main__":
     while data.running:
         data.APP.eventHandler(60)
+        
+        if gf.Interactions.isKeyReleased(K_ESCAPE):
+            data.running = False
+        
         page_listing[data.active_page.value]()  
